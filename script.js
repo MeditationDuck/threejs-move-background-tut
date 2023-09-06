0ba127280dd4726d31b8a1f380101f01b8c13de7
MeditationDuck <yoshinao@fit.cvut.cz>
2023-09-06 11:51:43 +0900

    feat: add codes

---
+++ script.js
@@ -0,0 +1,50 @@
+import * as THREE from 'three'
+import './style.css'
+import bg1 from './img/bg1.jpg'
+
+const container = document.querySelector('.three_bg')
+const loader = new THREE.TextureLoader()
+
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(70, window.innerWidth/window.innerHeight, 0.1, 1000)
+const renderer = new THREE.WebGL1Renderer()
+renderer.setPixelRatio(window.devicePixelRatio);
+
+renderer.setSize(window.innerWidth, window.innerHeight)
+container.appendChild(renderer.domElement)
+
+const geometry = new THREE.PlaneGeometry(14,8,5,9)
+const material = new THREE.MeshBasicMaterial({
+  // color: 0xff0000,
+  map: loader.load(bg1),
+})
+
+const mesh = new THREE.Mesh(geometry, material)
+
+scene.add(mesh)
+camera.position.z = 5
+
+const count = geometry.attributes.position.count
+console.log(count)
+const clock = new THREE.Clock()
+
+
+function animate() {
+  const time = clock.getElapsedTime()
+  for(let i = 0; i < count ;i++) {
+    const x = geometry.attributes.position.getX(i)
+    const y = geometry.attributes.position.getY(i)
+
+    const animl = 0.25 * Math.sin(x+time*0.7)
+    const anim2 = 0.35 * Math.sin(x+time*0.7)
+    const anim3 = 0.1 * Math.sin(x* 15+time*0.7)
+
+    geometry.attributes.position.setZ(i, animl + anim2 + anim3)
+    geometry.computeVertexNormals()
+    geometry.attributes.position.needsUpdate = true
+  }
+  requestAnimationFrame(animate)
+  renderer.render(scene, camera)
+}
+animate()
+
